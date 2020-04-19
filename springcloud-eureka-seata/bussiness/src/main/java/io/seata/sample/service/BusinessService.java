f3c83c90cad89012b643ba6bea8a65d038e65f7b
Evan <530788697@qq.com>
2020-04-19 20:20:35 +0800

    init

--- springcloud-eureka-seata/bussiness/src/main/java/io/seata/sample/service/BusinessService.java
+++ springcloud-eureka-seata/bussiness/src/main/java/io/seata/sample/service/BusinessService.java
@@ -39,29 +39,29 @@ public class BusinessService {
 
         orderFeignClient.create(userId, commodityCode, orderCount);
 
-        if (!validData()) {
-            throw new RuntimeException("账户或库存不足,执行回滚");
-        }
+        //if (!validData()) {
+        //    throw new RuntimeException("账户或库存不足,执行回滚");
+        //}
     }
 
-    @PostConstruct
-    public void initData() {
-        jdbcTemplate.update("delete from account_tbl");
-        jdbcTemplate.update("delete from order_tbl");
-        jdbcTemplate.update("delete from storage_tbl");
-        jdbcTemplate.update("insert into account_tbl(user_id,money) values('U100000','10000') ");
-        jdbcTemplate.update("insert into storage_tbl(commodity_code,count) values('C100000','200') ");
-    }
+    //@PostConstruct
+    //public void initData() {
+    //    jdbcTemplate.update("delete from account_tbl");
+    //    jdbcTemplate.update("delete from order_tbl");
+    //    jdbcTemplate.update("delete from storage_tbl");
+    //    jdbcTemplate.update("insert into account_tbl(user_id,money) values('U100000','10000') ");
+    //    jdbcTemplate.update("insert into storage_tbl(commodity_code,count) values('C100000','200') ");
+    //}
 
-    public boolean validData() {
-        Map accountMap = jdbcTemplate.queryForMap("select * from account_tbl where user_id='U100000'");
-        if (Integer.parseInt(accountMap.get("money").toString()) < 0) {
-            return false;
-        }
-        Map storageMap = jdbcTemplate.queryForMap("select * from storage_tbl where commodity_code='C100000'");
-        if (Integer.parseInt(storageMap.get("count").toString()) < 0) {
-            return false;
-        }
-        return true;
-    }
+    //public boolean validData() {
+    //    Map accountMap = jdbcTemplate.queryForMap("select * from account_tbl where user_id='U100000'");
+    //    if (Integer.parseInt(accountMap.get("money").toString()) < 0) {
+    //        return false;
+    //    }
+    //    Map storageMap = jdbcTemplate.queryForMap("select * from storage_tbl where commodity_code='C100000'");
+    //    if (Integer.parseInt(storageMap.get("count").toString()) < 0) {
+    //        return false;
+    //    }
+    //    return true;
+    //}
 }
